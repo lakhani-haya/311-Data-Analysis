b20b9d3f719827db04945f1e87bced1da26ec33a
Haya Lakhani <157073204+lakhani-haya@users.noreply.github.com>
2026-04-16 00:00:38 -0500

    Update kpi_top_categories.sql

--- sql/kpis/kpi_top_categories.sql
+++ sql/kpis/kpi_top_categories.sql
@@ -1,1 +1,7 @@
-
+CREATE OR REPLACE TABLE reporting.kpi_top_categories AS
+SELECT
+    category,
+    COUNT(*) AS total_requests
+FROM clean_data.service_requests
+GROUP BY category
+ORDER BY total_requests DESC;
